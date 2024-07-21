3984a602a28578a84f3433b12bbc5283d8a19a8f
rafaa1999 <raferhouma10@gmail.com>
2024-07-21 20:25:12 +0100

    Add some test for bookmark creator test:
    - Should create bookmark
    - Should return the bookmark after creating it
    - Should not be run the test using (@Ignore: annotation)

--- domain/src/test/java/com/rafaa/junit4/BookmarkCreatorTest.java
+++ domain/src/test/java/com/rafaa/junit4/BookmarkCreatorTest.java
@@ -1,16 +1,17 @@
 package com.rafaa.junit4;
 
 import com.rafaa.api.CreateBookmark;
+import com.rafaa.bookmark.Bookmark;
 import com.rafaa.bookmark.BookmarkCreator;
 import com.rafaa.bookmark.Bookmarks;
 import com.rafaa.stubs.InMemoryBookmarks;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.*;
 
+import java.util.Optional;
 import java.util.Set;
 
 import static java.util.Collections.singleton;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookmarkCreatorTest {
 
@@ -38,6 +39,32 @@ public class BookmarkCreatorTest {
     public void tear_down(){
         bookmarks = null;
         createBookmark = null;
+    }
+
+    @Test
+    public void should_create_bookmark(){
+        //given
+        createBookmark.forResource(url,name,tags);
+        Optional<Bookmark> saved = bookmarks.getBy(url);
+        //when
+        Bookmark expected = Bookmark.create(url,name,tags);
+        //then
+        assertThat(saved).hasValue(expected);
+    }
+
+    @Test
+    public void should_return_the_bookmark_after_creating_it(){
+        //given
+        Bookmark bookmark = createBookmark.forResource(url, name, tags);
+        //when
+        //then
+        assertThat(bookmark).isNotNull();
+    }
+
+    @Test
+    @Ignore
+    public void should_not_be_run(){
+        Assert.fail();
     }
 
 }
